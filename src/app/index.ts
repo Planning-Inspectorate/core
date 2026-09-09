96572fabf7788aafc4a09d5fa06ab30b4903b9aa
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-09-09 12:02:55 +0100

    fix: export app/csrf, auth/config, and auth folder

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,2 +1,3 @@
 export * from './app.ts';
 export * from './base-service.ts';
+export * from './csrf.ts';
--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,4 +1,5 @@
 export * from './auth-service.ts';
+export * from './config.ts';
 export * from './controller.ts';
 export * from './guards.ts';
 export * from './router.ts';
